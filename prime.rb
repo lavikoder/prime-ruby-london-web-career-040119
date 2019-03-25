a87160a22b9d35edafbe552c313e80d4fd57dfb5
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-25 08:29:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(p_num)
-array = []
-  #iterate and find if number can be divided by 2, 3, 5 and 7 
-  if (arr_num/2 || arr_num/3 || arr_num/5   
+range = (1..101)
+array = range.arr
+  #iterate and find if number can be divided by 2, 3, 5   and 7 
   
 end
 
